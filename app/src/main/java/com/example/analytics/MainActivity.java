7992246e2f5b110b44017fb26f9c4c9119258dae
Jirawat Karanwittayakarn <jirawatee@gmail.com>
2019-06-26 11:16:20 +0700

    Migrate to Android X

--- app/src/main/java/com/example/analytics/MainActivity.java
+++ app/src/main/java/com/example/analytics/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.analytics;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
